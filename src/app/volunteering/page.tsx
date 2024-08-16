6c2ad9fc76f248354a18311b6a68aa08ce0ec41f
Mentita <a01711870@tec.mx>
2024-08-16 09:58:41 -0600

    [ADD] Volunteering page

---
+++ src/app/volunteering/page.tsx
@@ -0,0 +1,11 @@
+const Volunteering = () => {
+    return (
+        <main>
+            <div className="">
+                <h1>Pagina de voluntariadoooo</h1>
+            </div>
+        </main>
+    );
+}
+
+export default Volunteering;
