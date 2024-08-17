643efcdfa2aa9c8b962a4c4d68e37844d3377960
SALV <a01707122@tec.mx>
2024-08-16 20:57:28 -0600

    [ADD] navbar branch

--- src/app/_components/Molecules/Navbar.tsx
+++ src/app/_components/Molecules/Navbar.tsx
@@ -5,9 +5,11 @@ import { navbar } from '../constants'
 const Navbar = () => {
   return (
     <div className='flex bg-secondary p-5 text-white justify-between items-center'>
+      {/* Logo */}
       <Link href={'/'}>
         <img src='icons/logo.png' className='w-32'/>
       </Link>
+      {/* Páginas de navegación: lo puse de esta manera para que sea más sencillo añadir valores o cambiarlos */}
       <nav className='flex gap-20'>
         {navbar.map((link) => (
           <Link href={link.href} className='hover:bg-black bg-opacity-10 rounded-md px-5 p-2'>
@@ -15,6 +17,7 @@ const Navbar = () => {
           </Link>
         ))}
       </nav>
+      {/* Página de donación */}
       <Link className='w-32 drop-shadow-md bg-third rounded-full text-center hover:bg-[rgb(255,40,40)] py-1' href={'/donar'}>
         Donar
       </Link>
