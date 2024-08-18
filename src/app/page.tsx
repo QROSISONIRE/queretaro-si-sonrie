50dff27ef0e5b27d42f4975a4adc59c7a2a1415c
Frida Arcadia <frida.arcadialv@gmail.com>
2024-08-18 14:27:55 -0600

    [ADD] images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
 export default function Home() {
 
   return (
-    <>
-    Inicio
-    </>
+    
+
+    <main className="relative min-h-screen items-center justify-center">
+
+       <section className="grid md:grid-cols-2 md:text-left min-h-screen flex-col items-start justify-start"> 
+        <div className="md:flex md:shrink-0 min-h-screen flex flex-col items-start justify-start">
+          <img 
+            src="/images/Collage.png" alt="imagen" 
+            />
+        </div>
+      </section>
+
+      <section className="grid md:grid-cols-2 md:text-left min-h-screen flex-col items-start justify-start"> 
+        <div className="md:flex md:shrink-0 min-h-screen flex flex-col items-start justify-start">
+            <img 
+              src="/images/elipse.png" alt="imagen" 
+              />
+          </div>
+      </section>
+    </main>
   );
 }
