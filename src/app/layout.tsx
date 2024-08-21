fe9cc7f0cca5158cbf6b20531304812e0176df38
SALV <a01707122@tec.mx>
2024-08-21 11:03:07 -0600

    [MERGE] main

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
 import { TRPCReactProvider } from "~/trpc/react";
-import Navbar from "./_components/Molecules/Navbar";
+import Navbar from "./_components/molecules/Navbar";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
