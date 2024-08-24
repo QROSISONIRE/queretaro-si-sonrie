d82d9fad0668e5d70fee4708c918374976475c79
Mentita <a01711870@tec.mx>
2024-08-23 20:06:22 -0600

    [ADD] Design adjustment

--- src/app/volunteering/page.tsx
+++ src/app/volunteering/page.tsx
@@ -61,28 +61,45 @@ const CorpusVolunteering = () => {
             </div>
 
               {/* Sección de requisitos y voluntariado */}
+            <div className='p-10 flex flex-row justify-evenly'>
+                <h1 className='text-5xl font-bold'>Voluntariado</h1>
+            </div>
+            <div className='flex flex-col p-10 pl-20 justify-center items-center'>
+                <p>Para poder ser voluntario es necesario ser especialista en cualquiera de las áreas de trabajo, 
+                tener experiencia en el manejo de los pacientes con LPH, donar tiempo y conocimientos. 
+                Saber trabajar en equipo y reportar información.
+                </p>                       
+            </div>
+            
             <div className='flex flex-row'>
-                <div className = 'bg-blue-400 p-10 w-1/2 flex flex-col justify-evenly items-center'>
-                    <div className='bg-blue-300 p-4'><img src='https://via.placeholder.com/270x360' alt='Imagen de la actividad'/></div>
-                    <div className='bg-blue-200 p-4 text-2xl'>Características</div>
-                    <div className='bg-blue-300 p-4 '>Pasión por su trabajo</div>
-                    <div className='bg-blue-200 p-4 '>Tener gusto por atender pacientes pediátricos</div>
-                    <div className='bg-blue-300 p-4 '>Gusto de trabajar en equipo</div>
-                    <div className='bg-blue-200 p-4 '>Colaborar en actividades extra en pro de la fundación</div>
+                <div className = 'w-1/2 '>
+                    <div className='bg-blue-950 text-white flex flex-col mt-14 p-4 m-10 rounded-3xl'>
+                        <div className='p-8 text-2xl font-bold'>¿Qué voy a realizar?</div> 
+                        <ul className="list-disc pl-12 pb-8">
+                            <li>Actividades profesionales en tu área de trabajo una vez por semana.</li>
+                            <li>Reporte de actividades mensuales en formato y fotografía.</li>
+                        </ul> 
+                    </div> 
+                    <div className='p-4 flex flex-col justify-evenly items-center mt-24'>
+                        <img src='images/Voluntariado.png' alt='Imagen de la actividad'/>
+                    </div>
+                        
                 </div>
-                
-                <div className = 'bg-blue-600 w-1/2'>
-                    <div className='bg-blue-800 p-8 text-white flex flex-row justify-evenly'>
-                    <h1 className='text-3xl'>Voluntariado</h1>
+
+                <div className = 'p-10 w-1/2'>
+                    <div className='p-4 flex flex-col justify-evenly items-center'>
+                        <img src='images/Voluntariado2.png' alt='Imagen de la actividad'/>
                     </div>
 
-                    <div className='flex flex-col p-4 justify-center items-center'>
-                        <div className='bg-blue-300 p-4 text-2xl'>¿Qué voy a realizar?</div>
-                        <div className='bg-blue-200 p-4 '>Actividades profesionales en tu área de trabajo una vez por semana</div>
-                        <div className='bg-blue-300 p-4 '>Reporte de actividades mensuales en formato y fotografía</div>
-                        <div className='bg-blue-200 p-4'><img src='https://via.placeholder.com/360x270' alt='Imagen de la actividad'/></div>
+                    <div className='bg-blue-950 text-white flex flex-col p-4 m-10 mt-11 rounded-3xl'>
+                        <div className='p-8 text-2xl font-bold'>Características</div>
+                        <ul className="list-disc pl-12 pb-8">
+                            <li>Pasión por su trabajo.</li>
+                            <li>Tener gusto por atender pacientes pediátricos.</li>
+                            <li>Gusto de trabajar en equipo.</li>
+                            <li>Colaborar en actividades extra en Pro de la fundación</li>
+                        </ul>
                     </div>
-
                 </div>
             </div>
 
