2c6c145571bb1f3656998511575a86711c7a3521
kamimartinez <kamiii.martineeez@gmail.com>
2024-08-28 14:23:46 -0600

    [FIX] Merge Conflicts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import Link from "next/link";
-import { LatestPost } from "~/app/_components/post";
-import { getServerAuthSession } from "~/server/auth";
-import { api, HydrateClient } from "~/trpc/server";
-
-export default async function Home() {
-  const hello = await api.post.hello({ text: "from tRPC" });
-  const session = await getServerAuthSession();
-
-  void api.post.getLatest.prefetch();
-
-  return (
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <Link href="/team">
-          <button className="bg-blue-500 text-white py-2 px-4 rounded">
-            Go to Team
-          </button>
-        </Link>
-      </main>
-  );
-}
-=======
 export default function Home() {
 
   return (
@@ -31,4 +8,3 @@ export default function Home() {
     
   );
 }
->>>>>>> main
