737b10d490c1e2c1047f60766a08bf4de5c46886
Frida Arcadia <frida.arcadialv@gmail.com>
2024-08-29 10:44:01 -0600

    [FIX] Simple Logo (no background)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -101,7 +101,7 @@ export default function Home() {
       {/* Organigrama */}
       <div className="flex flex-col items-center p-5 h-screen text-bold text-6xl poppins">
         <div className="flex h-fit items-center">
-          <img src={'icons/simpleLogo.png'} className="size-20"/>Organigrama
+          <img src={'icons/simplelogo.png'} className="size-20"/>Organigrama
         </div>
         <hr className="w-11/12 mt-5 border-2 border-black"/>
       </div>
